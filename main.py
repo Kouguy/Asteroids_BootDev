9922de310108f44c27949752e2a4a893fe556c11
kouguy <kouguy@free.fr>
2026-02-18 20:20:13 +0100

    modified:   circleshape.py 	modified:   constants.py 	modified:   main.py 	modified:   player.py Adding containers for Player

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock=pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()# Group object
+    drawable = pygame.sprite.Group()# Group object
+    Player.containers = (updatable, drawable) # Containers for Player
     PLAYER = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        PLAYER.update(dt)
-        PLAYER.draw(screen)
+                return 
+        updatable.update(dt)
+        screen.fill("black")        
+        for object in drawable:
+            object.draw(screen)                
         pygame.display.flip()
         Clock.tick(60)
         dt = Clock.get_time() / 1000
