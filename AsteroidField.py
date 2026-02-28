b899e46cd78a4e3cb6b648062939e53183ed99fa
kouguy <kouguy@free.fr>
2026-02-28 09:37:19 +0100

    modified: AsteroidField.py 	modified: README.md 	deleted: test.py

--- AsteroidField.py
+++ AsteroidField.py
@@ -56,4 +56,3 @@ class AsteroidField(pygame.sprite.Sprite):
             position = edge[1](random.uniform(0, 1))
             kind = random.randint(1, ASTEROID_KINDS)
             self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
-
--- test.py
+++
@@ -1,3 +0,0 @@
-def Hello_World():
-    print("Hello")
-    print("you ok")
